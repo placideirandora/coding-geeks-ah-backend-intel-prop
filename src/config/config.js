c24ef8f1e01624affe26007afef0d92c24d73233
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-08-02 17:27:59 +0200

    [Start #167484579] adding start script and Procfile for heroku

--- src/config/config.js
+++ src/config/config.js
@@ -20,8 +20,8 @@ module.exports = {
   'production': {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   }
 };
