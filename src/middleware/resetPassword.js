9b90c422ea5a88bba9c8a1a71cf71fb309800aae
Carlos Harerimana <carlosharerimana@Carloss-MacBook-Pro.local>
2019-08-08 19:50:27 +0200

    feat(reset password) User can reset password [Finishes #167484588]

--- src/middleware/resetPassword.js
+++ src/middleware/resetPassword.js
@@ -2,16 +2,12 @@ import jwt from 'jsonwebtoken';
 
 const verifyToken = (req, res, next) => {
   const { token } = req.params;
-  if (!token) {
-    res.status(401).json({ status: 401, error: 'Invalid token' });
-  } else {
-    jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
-      if (error) {
-        return res.status(403).json({ status: 403, error: `${error.message}` });
-      }
-      req.userData = decoded;
-      next();
-    });
-  }
+  jwt.verify(token, process.env.SECRET_KEY, (error, decoded) => {
+    if (error) {
+      return res.status(403).json({ status: 403, error: `${error.message}` });
+    }
+    req.userData = decoded;
+    next();
+  });
 };
 export default verifyToken;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import Validation from '../middleware/validation';
 import UserAuth from '../controllers/userController';
+import emailValidatator from '../middleware/emailValidation';
+import pwdValidator from '../middleware/passwordValidation';
+import resetPwd from '../middleware/resetPassword';
 
 const router = express.Router();
 
 router.post('/api/v1/users', Validation.signupValidation, UserAuth.signup);
+router.post('/api/v1/send-email', emailValidatator.emailValidation, UserAuth.sendEmail);
+router.post('/api/v1/reset-password/:token', resetPwd, pwdValidator.passwordValidation, UserAuth.resetPassword);
 
 export default router;
